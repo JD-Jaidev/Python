3c3b6fba58e93b879fc3d6f94374b166ddba699c
JD-Jaidev <artificialintelligencejai@gmail.com>
2026-07-04 14:21:25 +0530

    feat : added right triangle python pattern

--- Python patterns/right-triangle.py
+++ Python patterns/right-triangle.py
@@ -1,27 +1,49 @@
 # RIGHT TRIANGLE PATTERN
-n=5
-for i in range(1,n+1):
-    print(i*'*')
 
+def triangle_pattern(n):
+    for i in range(1,n + 1):
+        print(i * '*')
+triangle_pattern(5)
 print()
 
 # RIGHT TRIANGLE PATTERN INVERTED
-n=5
-for i in range(n,0,-1):
-    print(i*'*')
 
+def triangle_pattern_inverted(n):
+    for i in range(n,0,-1):
+        print(i * '*')
+triangle_pattern_inverted(5)
 print()
 
-# RIGHT TRIANGLE USING NUMBERS - 1
+# RIGHT TRIANGLE PATTERN USING NUMBERS - 1
 
-for i in range(1,6):
-    print(str(i) * i)
-
+def triangle_number_pattern1(n):
+    for i in range(1,6):
+        print(str(i) * i)
+triangle_number_pattern1(5)
 print()
 
-# RIGHT TRIANGLE USING NUMBERS - 2
-for i in range(1,6):
-    for j in range(1,i+1):
-        print(j,end='')
-    print()
+# RIGHT TRIANGLE PATTERN USING NUMBERS - 2
 
+def triangle_number_pattern2(n):
+    for i in range(1,6):
+        for j in range(1,i + 1):  
+            print(j,end = '')
+        print()
+triangle_number_pattern2(5)
+print()
+
+# RIGHT MIRROR TRIANGLE PATTERN
+
+def triangle_mirror_pattern(n):
+    for i in range(1,n+1):
+        print(' ' * (n - i) + '*' * i)
+triangle_mirror_pattern(5)
+print()
+
+# RIGHT MIRROR TRIANGLE PATTERN INVERTED
+
+def triangle_mirror_pattern_inverted(n):
+    for i in range(n,0,-1):
+        print(' ' * (n - i) + '*' * i)
+triangle_mirror_pattern_inverted(5)
+print()
