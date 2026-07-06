0a146130b680647f6f9d0f8ddcc279c48053d2b3
JD-Jaidev <artificialintelligencejai@gmail.com>
2026-07-06 10:16:28 +0530

    feat : hour glass pattern in python

---
+++ Python patterns/hour-glass.py
@@ -0,0 +1,8 @@
+# HOUR GLASS PATTERN
+
+def hour_glass(n):
+    for i in range(n,0,-1):
+        print(' ' * (n - i)+'*'*(2*i-1))
+    for j in range(2,(n+1)):
+        print(' ' * (n - j)+'*'*(2*j-1))
+hour_glass(5)
