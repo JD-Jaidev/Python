2cbfa908118677c815afbf58ac5ba2a0fffcddd6
JD-Jaidev <artificialintelligencejai@gmail.com>
2026-07-07 10:20:05 +0530

    feat : hollow number diamond pattern in python

---
+++ Python patterns/hollow-number-diamond.py
@@ -0,0 +1,25 @@
+# HOLLOW DIAMOND PATTERN
+
+n = 5
+def hollow_diamond_pattern(n):
+    # UPPER HALF
+    for i in range(1,(n + 1)):
+        print(" " * (n - i),end = '')
+        print('7',end = '')
+
+        if i > 1:
+            print(" " * (2 * i - 3),end = '')
+            print('7',end = '')
+        print()
+
+    #LOWER HALF
+    for j in range((n - 1),0,-1):
+        print(" " * (n - j), end="")
+        print('7', end="")
+
+        if j > 1:
+            print(" " * (2 * j - 3), end="")
+            print('7', end="")
+        print()
+
+hollow_diamond_pattern(5)
