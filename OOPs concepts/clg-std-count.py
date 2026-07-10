a25b7fdbb3c783534f14530608019a0a38cefaec
JD-Jaidev <artificialintelligencejai@gmail.com>
2026-07-10 12:24:40 +0530

    feat : college student count using class method program

---
+++ OOPs concepts/clg-std-count.py
@@ -0,0 +1,15 @@
+class college:
+    total = 0
+    def __init__(self,name):
+        self.name = name
+        college.total += 1
+    
+    @classmethod
+    def display_total(cls):
+        print(f'Total number of students : {cls.total}') 
+
+s1 = college('Jai')
+s2 = college('Dev')
+s3 = college('Thanish')
+
+college.display_total()
