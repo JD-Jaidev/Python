02dc0bc4c8955a91d344230c185ff024db20f25e
JD-Jaidev <artificialintelligencejai@gmail.com>
2026-07-10 11:09:22 +0530

    feat : student info system program

---
+++ OOPs concepts/student-info-system.py
@@ -0,0 +1,15 @@
+class student:
+    def __init__(self,name,roll,marks):
+        self.name = name
+        self.roll = roll
+        self.marks = marks
+    def show(self):
+        print(f'Student name : {self.name}')
+        print(f'Student roll no. : {self.roll}')
+        print(f'Student marks : {self.marks}')
+
+s1 = student('Jai',1,90)
+s1.show()
+
+s2 = student('Dev',2,80)
+s2.show()
