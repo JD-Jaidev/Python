4a74b85deb5d953ad7192af23b91508bc3ca10f8
JD-Jaidev <artificialintelligencejai@gmail.com>
2026-07-11 11:26:05 +0530

    feat : higher order functions in python

--- OOPs concepts/higher-order-func.py
+++ OOPs concepts/higher-order-func.py
@@ -1,3 +1,30 @@
+# 1. PASSING FUNCTIONS AS ARGUMENTS
+
+# Eg - 1
+def greet():
+    print('hello')
+def execute(func):
+    func()
+
+execute(greet)
+
+# Eg - 2
+
+def square(x):
+    return x**2
+def cube(x):
+    return x**3
+
+def perform(operation,number):
+    return operation(number)
+
+print(perform(square,4))
+print(perform(cube,4))
+
+
+# 2. RETURNING FUNCTIONS
+
+# Eg - 1
 def divisor(x):
     def dividend(y):
         return y / x
@@ -5,3 +32,12 @@ def divisor(x):
 
 div = divisor(2)
 print(div(10))
+
+# Eg - 2
+def operation():
+    def greet():
+        print('hello world')
+    return greet
+
+x = operation()
+x()
