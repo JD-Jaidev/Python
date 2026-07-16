ea83de52157f9f6a4285d3528e64f2c201e15c38
JD-Jaidev <artificialintelligencejai@gmail.com>
2026-07-16 09:51:20 +0530

    feat : frames in tkinter

---
+++ Tkinter-GUI/frames.py
@@ -0,0 +1,15 @@
+# FRAMES IN TKINTER - RECTANGULAR CONTAINER TO HOLD AND GROUP WIDGETS
+
+from tkinter import *
+
+window = Tk()
+
+frame = Frame(window,bg="pink",bd=5,relief=SUNKEN)
+frame.pack()
+
+Button(frame,text="W",font=("Consolas",25),width=3).pack(side=TOP)
+Button(frame,text="A",font=("Consolas",25),width=3).pack(side=LEFT)
+Button(frame,text="S",font=("Consolas",25),width=3).pack(side=LEFT)
+Button(frame,text="D",font=("Consolas",25),width=3).pack(side=LEFT)
+
+window.mainloop()
