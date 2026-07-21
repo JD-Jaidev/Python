7300e6bc9b60fec277fd982c4ecab2a318dbce5c
JD-Jaidev <artificialintelligencejai@gmail.com>
2026-07-21 19:46:59 +0530

    feat : using css like styles in PyQt5

---
+++ PyQt5/css-like-styles.py
@@ -0,0 +1,13 @@
+'''
+
+to add CSS like styles to widgets we can use the 
+self.setStyleSheet() where we cann the porperties like css. for induvidual widgets use the
+for eg - self.button1.setObjectName('button1')
+then use QPushButton#button1{
+                            # properties to be added
+}
+
+we can do this all together inside """ """
+we can also give other properties like hover
+
+'''
