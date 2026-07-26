bbe85378c73e3204e0e6fe5edb4631f22c86c153
JD-Jaidev <artificialintelligencejai@gmail.com>
2026-07-26 15:03:45 +0530

    feat : text widgets in streamlit

---
+++ Streamlit/text-widgets.py
@@ -0,0 +1,21 @@
+# INTRODUCTION TO STREAMLIT - Streamlit is an open-source Python framework used to build interactive web applications entirely using Python. 
+# It is especially popular among data scientists, AI/ML engineers, and Python developers because you don't need to learn HTML, CSS, or JavaScript to create a web interface.
+
+import streamlit as st
+
+# 1. Text display widgets
+
+st.title('First Streamlit program.') # Displays the main title of your Streamlit application.
+st.write('This a text.') # The most versatile display func. It can display almost everything,
+                         # Text, variables, dataframes, dictionaries, lists, images, charts.
+st.header('Streamlit') # Displays a large section heading.
+st.subheader('Tutorial') # Displays a heading smaller than header(). 
+st.text('Text tag') # Displays plain text, no markdown, no formatting.
+st.markdown('Bold text') # Displays markdown - bold, italic, headers, lists, tables, HTML.
+st.caption('Caption tag') # Displays small grey text useful for notes.
+st.code("print('Hello')",language = 'python') # Displays syntax highlighted code.
+st.latex("E = mc^2") # Displays mathematical equations.
+st.html() # Displays custom HTML inside """.
+st.json() # Displays json in a readable format.
+st.metric() # Shows KPI (Key performance indicator), useful in dashboards.
+st.badge('AI',color = 'blue') # Displays a small colored label (badge) to highlight status or categories.
