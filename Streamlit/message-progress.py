1f7abb5aceaef88e2f393b45a7c9249a9ec1670e
JD-Jaidev <artificialintelligencejai@gmail.com>
2026-07-26 15:46:35 +0530

    feat : message and progress in streamlit

---
+++ Streamlit/message-progress.py
@@ -0,0 +1,66 @@
+# MESSAGE AND PROGRESS WIDGETS - These widgets help you inform users, display errors, show progress, and indicate that a task is running.
+
+import streamlit as st
+import time
+
+st.success('Success',icon = '📈') # Displays a green success message.
+st.info('Info',icon = '📈') # Displays a blue info message.
+st.error('Error',icon = '📈') # Displays a red error message.
+st.warning('Warning',icon = '📈') # Displays a yellow warning message.
+# st.exception('Exception') # Displays a Python exception with its traceback. Useful while debugging.
+st.toast('Hello',icon = '📈') # Displays a small temporary notification. Disappears automatically.
+st.progress(75,text = 'Loading') # Shows how much work has been completed.
+
+# Eg program for progress - 
+
+progress = st.progress(0)
+for i in range(101):
+    time.sleep(0.05)
+    progress.progress(i,text = f'Loading : {i}')
+
+st.spinner() # Shows a loading spinner while code is running. Used when you don't know the exact progress percentage. 
+
+# Eg - 
+with st.spinner(text="Loading..."):
+    time.sleep(3)
+st.success('Doneee',icon = '📈')
+
+st.status('Finished') # Displays the status of a multi-step process. Unlike spinner, it can show each stage of the task.
+
+# Eg - 
+with st.status(
+    "Processing...",expanded = True):
+    st.write("Loading Data...")
+    time.sleep(2)
+    st.write("Cleaning Data...")
+    time.sleep(2)
+    st.write("Training Model...")
+    time.sleep(2)
+st.success("Finished!")
+
+
+with st.status(
+    "Working...",
+    expanded = True
+) as status:
+
+    st.write("Loading...")
+    time.sleep(2)
+
+    st.write("Saving...")
+    time.sleep(2)
+
+    status.update(
+        label = "Completed!",
+        state = "complete",
+        expanded = False
+    )
+
+st.empty() # Creates an empty placeholder that can later display or replace content.
+
+# Eg - 
+placeholder = st.empty()
+placeholder.write("Loading...")
+time.sleep(2)
+placeholder.success("Completed!")
+
