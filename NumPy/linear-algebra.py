c2e8891565a094c34913bd85edc8ce4cd55e3896
JD-Jaidev <artificialintelligencejai@gmail.com>
2026-07-31 11:55:53 +0530

    feat : linear algebra in numpy

--- NumPy/linear-algebra.py
+++ NumPy/linear-algebra.py
@@ -2,5 +2,20 @@ import numpy as np
 
 #------------------------------------------------------------------------------------------------------------------------------
 # LINEAR ALGEBRA 
-print(f'\Linear algebra ------------------------------------------------------------------------------------------------------------------------------')
+print(f'\nLinear algebra')
+
+# Linear Algebra is the branch of mathematics that deals with vectors, matrices, and matrix operations.
+# In AI, Machine Learning, Computer Vision, and Deep Learning, almost everything is represented as matrices (datasets, images, neural network weights, embeddings, etc.).
+
+#------------------------------------------------------------------------------------------------------------------------------ 
+# 1. Matrix multipplication
+print(f'\nMatrix multiplication ------------------------------------------------------------------------------------------------------------------------------')
+
+# 1. np.dot() -
+'''
+Performs the dot product.
+1D arrays → Dot product of vectors.
+2D arrays → Matrix multiplication.
+Higher dimensions → More advanced behavior.
+'''
 
