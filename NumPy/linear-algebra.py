2dffb041734d2bddb62a3f441e5e0ec6f5803ca0
JD-Jaidev <artificialintelligencejai@gmail.com>
2026-08-01 16:43:11 +0530

    feat : linear algebra in numpy

--- NumPy/linear-algebra.py
+++ NumPy/linear-algebra.py
@@ -50,7 +50,7 @@ print(f'\n@ Operator : {a @ b}')
 # 2. Matrix transformation
 print(f'\nMatrix transformation ------------------------------------------------------------------------------------------------------------------------------')
 
-# transpose (.T) - Swaps rows and columns. or print(np.transpose(a))
+# np.transpose (.T) - Swaps rows and columns. or print(np.transpose(a))
 a = np.array([[1,2,3],
               [4,5,6]])
 print(f'\nTranspose : {a.T}')
@@ -58,3 +58,52 @@ print(f'\nTranspose : {a.T}')
 #------------------------------------------------------------------------------------------------------------------------------ 
 # 3. Vector operations
 print(f'\nVector operations ------------------------------------------------------------------------------------------------------------------------------')
+
+# 1. np.inner(array1 , array2) -  Computes the inner product (similar to dot product for 1D arrays).
+
+a = np.array([1,2,3])
+b = np.array([4,5,6])
+print(f'\nInner : {np.inner(a,b)}')
+
+# 2. np.outer(array1 , array2) - Computes the outer product. Each element of one vector is multiplied by every element of the other vector.
+
+a = np.array([1,2,3])
+b = np.array([4,5])
+print(f'\nOuter : {np.outer(a,b)}')
+
+'''
+1 × [4,5]
+2 × [4,5]
+3 × [4,5]
+'''
+
+# 3. np.cross(array1 , array2) - Computes the cross product of 3D vectors.
+
+a = np.array([1,0,0])
+b = np.array([0,1,0])
+print(f'\nCross : {np.cross(a,b)}')
+
+'''
+if a = [a₁, a₂, a₃] & b = [b₁, b₂, b₃]
+
+then a × b =
+[
+ a₂b₃ - a₃b₂,
+ a₃b₁ - a₁b₃,
+ a₁b₂ - a₂b₁
+]
+'''
+
+'''
+Common in -
+Physics
+Robotics
+Computer Graphics
+'''
+
+#------------------------------------------------------------------------------------------------------------------------------ 
+# 4. numpy.linalg
+print(f'\nnumpy.linalg ------------------------------------------------------------------------------------------------------------------------------')
+# Contains advanced linear algebra functions.
+
+import numpy.linalg as la
