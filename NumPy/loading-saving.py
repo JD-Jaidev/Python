ec0340588edc347e8f4539df66bc8b5c270e12f0
JD-Jaidev <artificialintelligencejai@gmail.com>
2026-08-02 10:34:01 +0530

    feat : loading & saving datas in numpy

---
+++ NumPy/loading-saving.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+print('\nLoading & Saving data in NumPy')
+
+'''
+Loading and saving data means storing NumPy arrays in files so you can use them later without recreating them.
+
+NumPy provides functions to save arrays in :
+Binary format (.npy) → Faster and preserves data types.
+Text format (.txt, .csv) → Human-readable and easy to share.
+'''
